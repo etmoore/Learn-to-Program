942e52fcda9ab2a33338a18f87cf8c909499e303
Evan Moore <etmoore@gmail.com>
2014-09-12 00:22:51 -0600

    finished exercise 2.5

---
+++ calc.rb
@@ -0,0 +1,31 @@
+# floats
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+
+# integers
+puts 1 + 2
+puts 2 * 3
+puts 5 - 8
+puts 9 / 2 # ruby will round this down
+
+# some more complicated arithmetic
+puts 5 * (12-8) + -15
+puts 98 + (59872 / (13*8)) * -51
+
+# hours in a year
+puts "hour in a year"
+puts 24 * 365.25
+
+# minutes in a decade
+puts "minutes in a decade"
+puts 60 * 24 * 365.25 * 10
+
+# my age in seconds
+puts "my age in seconds"
+puts 27 * 365.25 * 24 * 60 * 60
+
+# authors age
+puts "authors age"
+puts 1025000000 / 60 / 60 / 24 / 365.24
