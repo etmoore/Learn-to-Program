73f0476940f1cb9ee39ab04255e94f5017b6999b
Evan Moore <etmoore@gmail.com>
2014-09-13 17:31:49 -0600

    played with some conversions

---
+++ conversions.rb
@@ -0,0 +1,24 @@
+
+var1 = 2
+puts var1
+var2 ='5'
+puts var2
+
+puts var1.to_s
+puts var2.to_i # converts to integer
+
+puts var2.to_i + var1
+
+puts var2.to_f # converts to float
+
+
+puts '15'.to_f
+puts '99.999'.to_f
+puts '99.999'.to_i
+puts ''
+puts '5 is my favorite number!'.to_i # => 5
+puts 'Who asked you about 5 or whatever?'.to_i # => 0
+puts 'Your momma did.'.to_f # => 0.0
+puts ''
+puts 'stringy'.to_s
+puts 3.to_i
