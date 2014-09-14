8f183747b4d86f211413e3987fb43007b55fbec6
Evan Moore <etmoore@gmail.com>
2014-09-13 18:25:01 -0600

    finished through exercise 6.2

---
+++ chapter 6/angry_boss.rb
@@ -0,0 +1,3 @@
+puts 'Whaddya want?'
+desire = gets.chomp
+puts 'WHADDAYA MEAN "' + desire.upcase + '"?!? YOU\'RE FIRED!!'
---
+++ chapter 6/fancy_string_methods.rb
@@ -0,0 +1,19 @@
+var1 = 'The quick brown fox'
+puts var1
+puts var1.reverse
+
+letters = 'aAbBcCdDeE'
+puts letters.upcase
+puts letters.capitalize
+puts letters.downcase
+puts letters
+
+line_width = 50
+puts( 'Old Mother Hubbard'.center(line_width))
+puts( 'Sat in her cupboard'.center(line_width))
+puts( 'Eating her curds and whey'.center(line_width))
+
+line_width = 40
+puts( 'Old Mother Hubbard'.ljust(line_width))
+puts( 'Sat in her cupboard'.center(line_width))
+puts( 'Eating her curds and whey'.rjust(line_width))
