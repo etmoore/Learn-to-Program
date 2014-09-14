689494ae5344579f08006397cfe7b550c7364184
Evan Moore <etmoore@gmail.com>
2014-09-13 18:26:36 -0600

    removed older file

--- better_favorite_number.rb
+++
@@ -1,6 +0,0 @@
-puts 'What\'s your favorite number?'
-favorite_number = gets.chomp.to_i
-
-bigger_better_number = favorite_number + 1
-
-puts 'Hmmm... how about a bigger, better number? Your new favorite number is ' + bigger_better_number.to_s
