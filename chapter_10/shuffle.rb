1234fc69bfd80c78b24fbc1c8340fd030ad46ad2
Evan Moore <etmoore@gmail.com>
2014-10-01 14:03:07 -0600

    finished dictionary_sort

--- chapter_10/shuffle.rb
+++ chapter_10/shuffle.rb
@@ -3,8 +3,9 @@ def shuffle arr
 
   # loop through arr
   while arr.length > 0
-    # grab a random item from arr, delete it (returns the deleted value)
+    # grab a random item from arr
     random_position = rand(arr.length)
+    # delete it (returns the deleted value)
     random_object = arr.delete_at(random_position)
     # and push it into shuf
     shuf.push random_object
